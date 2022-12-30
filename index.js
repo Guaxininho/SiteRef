3fa14aed9c8a8007d395fa3cbba217510dc672b8
Kiiroi <sammydevjs@gmail.com>
2022-12-30 08:37:41 -0300

    botao ver mais

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Variavéis
 const cards = document.querySelectorAll('.cardExp'); // aqui seleciona todos os cards
 const conteudo = document.querySelectorAll('.verMais');
+const verMais = document.querySelectorAll('.clikvermais');
 
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', ()=>{
+    verMais[i].classList.toggle('naoVer');
     conteudo[i].classList.toggle('ver');
   })
 
